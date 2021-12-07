435797fa60407826bdf83422c426270f148a6a78
2300652404@qq.com <2300652404@qq.com>
2021-12-07 23:58:54 +0800

    verson1.0

--- 介绍Web/js/main.js
+++ 介绍Web/js/main.js
@@ -1,4 +1,5 @@
-var second;
+let second;
+
 function img(){
     let img=document.getElementById("img0");
     img.style.border="2px solid #20b39c";
@@ -6,7 +7,7 @@ function img(){
     setInterval(jumppage,500);
 }
 function jumppage(){
-    if(second==0){
+    if(second===0){
         let img=document.getElementById("img0");
         img.style.border="2px solid black";
         location.href="../resource/086.jpg";
@@ -22,4 +23,72 @@ function edit(){
     else
         bt.style.color="palegreen";
 
+}
+
+window.onload= function (){
+    let timer;
+    let imgList = document.getElementById("imgList");
+    let imgArr = document.getElementsByTagName("img");
+    let navId = document.getElementById("navId");
+    let outer = document.getElementById("outer");
+    imgList.style.width=520*(imgArr.length-1)+"px";
+    navId.style.left=(outer.offsetWidth-navId.offsetWidth)/2+"px";
+    let alla = document.getElementById("navId");
+    let allA = alla.children;
+    let index = 0;
+    allA[index].style.backgroundColor="black";
+    for(let i=0; i<allA.length; i++){
+        allA[i].num=i;
+        allA[i].onclick=function (){
+            clearInterval(timer);
+            index=this.num;
+            setA();
+            move(imgList,"left",-520*index,50,function (){
+                autoChange();
+            });
+        };
+    }
+    function setA(){
+        if(index>=imgArr.length-2){
+            index=0;
+            imgList.style.left="0";
+        }
+        for(let i=0;i<allA.length;i++){
+            allA[i].style.backgroundColor="";
+        }
+        allA[index].style.backgroundColor="black";
+    }
+
+    function autoChange(){
+        timer=setInterval(function (){
+            index++;
+            index%=imgArr.length-1;
+            move(imgList,"left",-520*index,20,function(){
+                setA();
+            });
+        },2000);
+    }
+    autoChange();
+    function move(obj,attr,target,speed,callback){
+        let current = parseInt(getStyle(obj,attr));
+        if(current>target){
+            speed=-speed;
+        }
+        clearInterval(obj.timer);
+        obj.timer=setInterval(function (){
+            let oldValue = parseInt(getStyle(obj,attr));
+            let newValue=oldValue+speed;
+            if((speed<0&&newValue<=target)||(speed>0&&newValue>=target)){
+                clearInterval(obj.timer)
+                callback&&callback();
+            }
+        },30);
+    }
+    function getStyle(obj,name){
+        if(window.getComputedStyle){
+            return getComputedStyle(obj,null)[name];
+        }else{
+            return obj.style[name];
+        }
+    }
 }
