9a4fcd112a719d2f9939913f0eefca3b27debdf3
2300652404@qq.com <2300652404@qq.com>
2021-12-10 14:12:56 +0800

    verson1.0

--- 介绍Web/js/main.js
+++ 介绍Web/js/main.js
@@ -1,94 +1,143 @@
 let second;
 
-function img(){
-    let img=document.getElementById("img0");
-    img.style.border="2px solid #20b39c";
-    second=1;
-    setInterval(jumppage,500);
+function img() {
+    let img = document.getElementById("img0");
+    img.style.border = "2px solid #20b39c";
+    second = 1;
+    setInterval(jumppage, 250) ;
 }
-function jumppage(){
-    if(second===0){
-        let img=document.getElementById("img0");
-        img.style.border="2px solid black";
-        location.href="../resource/086.jpg";
+
+function jumppage() {
+    if (second === 0) {
+        let img = document.getElementById("img0");
+        img.style.border = "2px solid black";
+        location.href = "../resource/086.jpg";
     }
     second--;
 }
-function edit(){
-    let bt=document.getElementById("edit");
-    bt.style.color="green";
+
+function edit() {
+    let bt = document.getElementById("edit");
+    bt.style.color = "green";
     let flag = window.confirm("确认退出吗？");
-    if(flag)
-        location.href="index.html";
+    if (flag)
+        location.href = "index.html";
     else
-        bt.style.color="palegreen";
+        bt.style.color = "palegreen";
 
 }
 
-window.onload= function (){
-    let timer;
-    let imgList = document.getElementById("imgList");
-    let imgArr = document.getElementsByTagName("img");
-    let navId = document.getElementById("navId");
-    let outer = document.getElementById("outer");
-    imgList.style.width=520*(imgArr.length-1)+"px";
-    navId.style.left=(outer.offsetWidth-navId.offsetWidth)/2+"px";
-    let alla = document.getElementById("navId");
-    let allA = alla.children;
-    let index = 0;
-    allA[index].style.backgroundColor="black";
-    for(let i=0; i<allA.length; i++){
-        allA[i].num=i;
-        allA[i].onclick=function (){
+window.onload = function () {
+    var TAG_l = document.getElementById("left");
+    var TAG_r = document.getElementById("right");
+    //获得ul的元素
+    var imgList = document.getElementById("imgList");
+    //获得图片的数组
+    var imgArr = document.getElementsByClassName("img1");
+    var navId = document.getElementById("navId");
+    var outer = document.getElementById("outer");
+    imgList.style.width = 520 * (imgArr.length) + "px";
+    //设置navId的位置 使其居中
+    navId.style.left = (outer.offsetWidth - navId.offsetWidth) / 2 + "px";
+    //得到所有的a 标签 如果有其他的A的话  这里需要注意要使用navId子元素的a
+    var alla = document.getElementById("navId");
+    var allA = alla.children;
+    var index = 0;
+    allA[index].style.backgroundColor = 'black';//设置默认的a为黑色
+    for (var i = 0; i < allA.length; i++) {
+        allA[i].num = i;
+        //alert(allA[i].num);
+        allA[i].onclick = function () {
             clearInterval(timer);
-            index=this.num;
+            index = this.num;
+            /* imgList.style.left= -520*index+"px"; */
             setA();
-            move(imgList,"left",-520*index,50,function (){
+            move(imgList, "left", -520 * index, 50, function () {
                 autoChange();
             });
         };
     }
-    function setA(){
-        if(index>=imgArr.length-2){
-            index=0;
-            imgList.style.left="0";
-        }
-        for(let i=0;i<allA.length;i++){
-            allA[i].style.backgroundColor="";
-        }
-        allA[index].style.backgroundColor="black";
+    TAG_l.onclick = function () {
+        if (--index < 0)
+            index = allA.length - 1;
+        clearInterval(timer);
+        setA();
+        move(imgList, "left", -520 * index, 50, function () {
+            autoChange();
+        });
+    }
+    TAG_r.onclick = function () {
+        if (++index > allA.length - 1)
+            index = 0;
+        clearInterval(timer);
+        setA();
+        move(imgList, "left", -520 * index, 50, function () {
+            autoChange();
+            console.log(index);
+        });
     }
 
-    function autoChange(){
-        timer=setInterval(function (){
+    function setA() {
+        //alert(index);
+        //当index值比图片的数目多的时候 就归0
+        if (index >= imgArr.length - 1) {
+            index = 0;
+            imgList.style.left = "0";
+        }
+        for (var i = 0; i < allA.length; i++) {
+            //去掉未点击的颜色  仍然保留a : hover有用
+            allA[i].style.backgroundColor = "";
+        }
+        allA[index].style.backgroundColor = "black";
+    }
+
+    var timer;
+
+    function autoChange() {
+        timer = setInterval(function () {
             index++;
-            index%=imgArr.length-1;
-            move(imgList,"left",-520*index,20,function(){
+            index %= imgArr.length;
+            move(imgList, "left", -520 * index, 20, function () {
                 setA();
             });
-        },2000);
+        }, 3000);
     }
+
     autoChange();
-    function move(obj,attr,target,speed,callback){
-        let current = parseInt(getStyle(obj,attr));
-        if(current>target){
-            speed=-speed;
+
+//可以根据 target 参数进行判断 向哪个方向移动
+    function move(obj, attr, target, speed, callback) {
+        var current = parseInt(getStyle(obj, attr));
+        //alert(current);
+        //根据目标的位置来判定 speed的值是正是负
+        if (current > target) {
+            speed = -speed;
         }
+        //自定义对象定时器 防止对象之间的混乱操作
         clearInterval(obj.timer);
-        obj.timer=setInterval(function (){
-            let oldValue = parseInt(getStyle(obj,attr));
-            let newValue=oldValue+speed;
-            if((speed<0&&newValue<=target)||(speed>0&&newValue>=target)){
-                clearInterval(obj.timer)
-                callback&&callback();
+        //alert(oldValue);
+        obj.timer = setInterval(function () {
+            var oldValue = parseInt(getStyle(obj, attr));
+            var newVal = oldValue + speed;
+            //如果移动的越界 进行重置
+            if ((speed < 0 && newVal <= target) || (speed > 0 && newVal >= target)) {
+                newVal = target;
             }
-        },30);
+            obj.style[attr] = newVal + "px";
+            if (newVal == target) {
+                clearInterval(obj.timer);
+                callback && callback();//回掉函数 先判断 有就执行 没有不执行
+            }
+        }, 30);
     }
-    function getStyle(obj,name){
-        if(window.getComputedStyle){
-            return getComputedStyle(obj,null)[name];
-        }else{
-            return obj.currentstyle[name];
+
+    //obj:获取样式元素
+    //name:获取样式名
+    function getStyle(obj, name) {
+        if (window.getComputedStyle) {
+            return getComputedStyle(obj, null)[name];
+        } else {
+            return obj.currentStyle[name];
         }
     }
 }
